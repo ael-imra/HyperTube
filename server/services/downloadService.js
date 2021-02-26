12ccdf72c2412e5b28c9aeb1b042df4616a4cee5
Abdellah El imrani <ael-imra@e2r12p1.1337.ma>
2021-02-26 19:42:08 +0100

    26-02-2021 fix error

--- server/services/downloadService.js
+++ server/services/downloadService.js
@@ -3,44 +3,61 @@ const http = require('http')
 const fs = require('fs')
 const path = require('path')
 const torrentStream = require('torrent-stream')
+const download = require('download')
+var srt2vtt = require('srt-to-vtt')
 
 const downloadSubtitles = async function (imdbID) {
 	const OpenSubtitles = new OS({
 		useragent: 'UserAgent',
 		username: '1337Hypertube',
 		password: 'ael-imra@1337',
-		ssl: true,
 	})
 	const subtitles = await OpenSubtitles.search({
 		imdbid: imdbID,
 		extensions: ['vtt', 'srt'],
 	})
-	Object.keys(subtitles).map((key) => {
+	Object.keys(subtitles).map(async (key) => {
 		if (key === 'fr' || key === 'en') {
-			const file = fs.createWriteStream(path.join(__dirname, '..', 'downloads/subtitles', `${imdbID}_${subtitles[key].lang}${path.extname(subtitles[key].filename)}`))
-			http.get(subtitles[key].url).then((response) => response.pipe(file))
+			const dirPath = path.join(__dirname, '..', 'downloads/subtitles')
+			const filePath = path.join(dirPath, `${imdbID}_${key}.vtt`)
+			if (!fs.existsSync(dirPath))
+				fs.mkdir(dirPath, () => {
+					fs.writeFile(filePath, '', (err) => (!err ? download(subtitles[key].url).pipe(srt2vtt()).pipe(fs.createWriteStream(filePath)) : 0))
+				})
+			else fs.writeFile(filePath, '', (err) => (!err ? download(subtitles[key].url).pipe(srt2vtt()).pipe(fs.createWriteStream(filePath)) : 0))
 		}
 	})
 }
 
 const downloadStream = async function (torrentHash, completeDownload) {
 	return new Promise((resolve) => {
+		let counter = 0
 		const engine = torrentStream(torrentHash, {
 			path: __dirname + '/../downloads/videos',
 		})
-		let sizeFiles = 0
+		// console.log('HELLO', torrentHash, engine)
+		// let sizeFiles = 0
 		engine.on('ready', () => {
 			/**
 			 * sort files desc
 			 * portended the large file is video
 			 */
+			console.log('ready')
 			engine.files.sort((file1, file2) => file2.length - file1.length)
-			engine.files.map((f) => (sizeFiles += f.length))
+			// engine.files.map((f) => (sizeFiles += f.length))
 			const file = engine.files[0]
 			resolve({ file })
 		})
-		engine.on('download', () => {
-			if (sizeFiles === engine.swarm.downloaded) completeDownload()
+		engine.on('idle', () => {
+			console.log('idle')
+		})
+		// engine.on('download', (index, buffer) => {
+		// 	console.log(index, buffer.toString(), 'OK')
+		// 	if (sizeFiles === engine.swarm.downloaded) completeDownload()
+		// })
+		engine.on('verify', (index) => {
+			counter++
+			console.log(index, 'verify', counter, engine.torrent.pieces.length)
 		})
 	})
 }
