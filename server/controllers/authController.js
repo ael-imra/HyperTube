54622e3633f97445a421c19732a0c8d4ea41a480
Soufiane El hamri <sel-hamr@e2r1p1.1337.ma>
2021-03-12 09:09:33 +0100

    merge server with client

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -4,6 +4,7 @@ const bcrypt = require('bcrypt')
 const { getJWT } = require('../services/userService')
 const { sendMail } = require(__dirname + '/../helper/mailHelper')
 const { getUser, insertLocalUser, updateUser, checkUserExist } = require(__dirname + '/../models/userModel')
+const { clientPort } = require(__dirname + '/../configs/indexConfig')
 
 const register = async function (req, res, next) {
 	try {
@@ -18,7 +19,7 @@ const register = async function (req, res, next) {
 		const checkResult = await checkUserExist(userName, email)
 		if (!checkResult) {
 			const token = generateToken(128)
-			sendMail('active', email, userName, `http://${req.headers.host}/`, token)
+			sendMail('active', email, userName, `http://localhost:1337/auth/active`, token)
 			req.body.password = await bcrypt.hash(password, 5)
 			const insertSuccessful = await insertLocalUser({ ...req.body, userFrom: 'local', token })
 			if (insertSuccessful)
@@ -105,29 +106,68 @@ const resetPassword = async function (req, res, next) {
 		const { email } = req.body
 		const user = await getUser({ email }, ['userFrom', 'userName', 'isActive', 'token'])
 		if (user & user.userFrom)
+			return res.redirect(
+				`http://localhost:${clientPort}/failed/${JSON.stringify({
+					Eng: 'Cannot reset password with this email',
+					Fr: 'Impossible de réinitialiser le mot de passe avec cet e-mail',
+				})}`
+			)
+		if (user) {
+			if (!user.isActive)
+				return res.redirect(
+					`http://localhost:${clientPort}/failed/${JSON.stringify({
+						Eng: 'Please verify your email than reset your password',
+						Fr: 'Veuillez vérifier votre adresse e-mail avant de réinitialiser votre mot de passe',
+					})}`
+				)
+			sendMail('reset', email, user.userName, `http://localhost:${clientPort}/ResetPassword/${user.token}`)
+			return res.redirect(
+				`http://localhost:${clientPort}/success/${JSON.stringify({
+					Eng: 'Check your email to reset password',
+					Fr: 'Vérifiez votre messagerie pour réinitialiser le mot de passe',
+				})}`
+			)
+		}
+		return res.redirect(`http://localhost:${clientPort}/failed/${JSON.stringify({ Eng: 'Incorrect email', Fr: 'Adresse Email incorrecte' })}`)
+	} catch (err) {
+		next(err)
+	}
+}
+const updatePassword = async function (req, res, next) {
+	try {
+		const { token, oldPassword, newPassword } = req.body
+		const tokenError = checkUserInput({ token }, ['token'])
+		const newPasswordError = checkUserInput({ password: newPassword }, ['password'])
+		const oldPasswordError = checkUserInput({ password: oldPassword }, ['password'])
+		if (tokenError || newPasswordError || oldPasswordError)
 			return res.send({
 				type: 'error',
 				status: 400,
-				body: { Eng: 'Cannot reset password with this email', Fr: 'Impossible de réinitialiser le mot de passe avec cet e-mail' },
+				body: tokenError || newPasswordError || oldPasswordError,
 			})
-		if (user) {
-			if (!user.isActive)
+		const user = await getUser({ token }, ['password', 'userID'])
+		console.log(user, 'TOKEN')
+		if (user && user.password) {
+			return bcrypt.compare(oldPassword, user.password, async (err, compare) => {
+				console.log(compare, 'comp')
+				if (err || !compare)
+					return res.send({
+						type: 'error',
+						status: 400,
+						body: { Eng: "old password doesn't match", Fr: "l'ancien mot de passe ne correspond pas" },
+					})
+				updateUser(user.userID, { password: await bcrypt.hash(newPassword, 5) })
 				return res.send({
-					type: 'warning',
-					status: 403,
-					body: { Eng: 'Please verify your email than reset your password', Fr: 'Veuillez vérifier votre adresse e-mail avant de réinitialiser votre mot de passe' },
+					type: 'error',
+					status: 400,
+					body: { Eng: 'Updated successful', Fr: 'Mise à jour réussie' },
 				})
-			sendMail('reset', email, user.userName, `http://${req.headers.host}/ResetPassword/${user.token}`)
-			return res.send({
-				type: 'success',
-				status: 200,
-				body: { Eng: 'Check your email to reset password', Fr: 'Vérifiez votre messagerie pour réinitialiser le mot de passe' },
 			})
 		}
 		return res.send({
 			type: 'error',
 			status: 400,
-			body: { Eng: 'Incorrect email', Fr: 'Adresse Email incorrecte' },
+			body: { Eng: 'Invalid token', Fr: 'Jeton invalide' },
 		})
 	} catch (err) {
 		next(err)
@@ -135,18 +175,18 @@ const resetPassword = async function (req, res, next) {
 }
 const activeAccount = async function (req, res, next) {
 	try {
-		const error = checkUserInput(req.body, ['token'])
+		const error = checkUserInput(req.params, ['token'])
 		if (error)
 			return res.send({
 				type: 'error',
 				status: 400,
 				body: error,
 			})
-		const { token } = req.body
-		const user = await getUser({ token }, 'userName')
+		const { token } = req.params
+		const user = await getUser({ token }, 'userID')
 		if (user) {
 			const token = generateToken(128)
-			updateUser(req.user, { token, isActive: 1 })
+			updateUser(user.userID, { token, isActive: 1 })
 			return res.send({
 				type: 'success',
 				status: 200,
@@ -162,9 +202,37 @@ const activeAccount = async function (req, res, next) {
 		next(err)
 	}
 }
+const checkToken = async function (req, res, next) {
+	try {
+		const error = checkUserInput(req.params, ['token'])
+		if (error)
+			return res.send({
+				type: 'error',
+				status: 400,
+				body: error,
+			})
+		const { token } = req.params
+		const user = await getUser({ token }, 'userID')
+		if (user)
+			return res.send({
+				type: 'success',
+				status: 200,
+				body: true,
+			})
+		return res.send({
+			type: 'error',
+			status: 200,
+			body: false,
+		})
+	} catch (err) {
+		next(err)
+	}
+}
 module.exports = {
 	register,
 	login,
 	resetPassword,
+	updatePassword,
 	activeAccount,
+	checkToken,
 }
