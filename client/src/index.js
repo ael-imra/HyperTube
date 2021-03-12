8b9432dd02fcb8ab42eb58164411cb151f68e075
Soufiane El hamri <sel-hamr@e2r1p1.1337.ma>
2021-03-12 09:17:29 +0100

    fix <App/>

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./Components/App";
 import Axios from "axios";
-import { LiteYouTubeEmbed } from "react-lite-youtube-embed";
 Axios.defaults.baseURL = "http://localhost:1337";
 
-ReactDOM.render(<LiteYouTubeEmbed id='L2vS_050c-M' title='What’s new in Material Design for the web (Chrome Dev Summit 2019)' noCookie={true} />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
