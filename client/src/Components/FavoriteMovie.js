f5d525a99df644d4b9a28154dac380d46200b1f5
Soufiane El hamri <sel-hamr@e2r1p1.1337.ma>
2021-03-13 11:25:19 +0100

    fix Error 13/4

--- client/src/Components/FavoriteMovie.js
+++ client/src/Components/FavoriteMovie.js
@@ -1,27 +1,31 @@
-import React from 'react';
-import { GetListMovieFavorite } from '../Assets/GetListMovieFavorite';
-import '../Css/FavoriteMovie.css';
-import CircularProgress from '@material-ui/core/CircularProgress';
-import { MovieCart } from './MovieCart';
-import noData from '../Images/no-data.svg';
+import React from "react";
+import { GetListMovieFavorite } from "../Assets/GetListMovieFavorite";
+import "../Css/FavoriteMovie.css";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import { MovieCart } from "./MovieCart";
+import { DataContext } from "../Context/AppContext";
+import noData from "../Images/no-data.svg";
 export const FavoriteMovie = () => {
   const [favoriteMovie, setFavoriteMovie] = React.useState(false);
+  const ctx = React.useContext(DataContext);
   React.useEffect(() => {
+    let unmount = false;
     const dataMovies = async () => {
-      const test = await GetListMovieFavorite();
-      setFavoriteMovie(test);
+      const moviesFavorite = await GetListMovieFavorite();
+      if (!unmount) setFavoriteMovie(moviesFavorite);
     };
-    dataMovies();
+    if (!unmount) dataMovies();
+    return () => (unmount = true);
   }, []);
   return (
-    <div className='Favorite' style={{ height: favoriteMovie instanceof Array ? 'auto' : '90%' }}>
+    <div className='Favorite' style={{ height: favoriteMovie instanceof Array ? "auto" : "90%" }}>
       {favoriteMovie ? (
         favoriteMovie instanceof Array ? (
           favoriteMovie.map((movie, key) => <MovieCart movie={movie} key={key} setFavoriteMovie={setFavoriteMovie} />)
         ) : (
           <div className='NoData'>
             <img src={noData} className='ImageNoData' />
-            <p>Ups!... no results found</p>
+            <p>{ctx.Languages[ctx.Lang].NoResult}</p>
           </div>
         )
       ) : (
