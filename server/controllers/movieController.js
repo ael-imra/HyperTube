d47e42ba90e214926a33bfba4e97ee7b11acae0a
Abdellah El imrani <ael-imra@e2r1p3.1337.ma>
2021-03-14 19:20:24 +0100

    14-03-2021

--- server/controllers/movieController.js
+++ server/controllers/movieController.js
@@ -24,54 +24,41 @@ const getMovieInfo = async function (req, res, next) {
 		const { imdbID } = req.params
 		if (typeof imdbID === 'string') {
 			const codeMovie = await axios.get(`https://yts.megaproxy.info/api/v2/list_movies.json?query_term=${imdbID}`)
-			const movieData = await axios.get(`https://yts.megaproxy.info/api/v2/movie_details.json?movie_id=${codeMovie.data.data.movies[0].id}&with_images=true`)
-			const movieInfo = movieData.data.data.movie
-			const { images, credits } = await getMovieImagesAndCasts(imdbID)
-			const movies = await axios.get(`https://yts.megaproxy.info/api/v2/movie_suggestions.json?movie_id=${codeMovie.data.data.movies[0].id}`)
-			let suggestions = movies.data.data.movies
-			suggestions = suggestions.map((movie) => ({
-				image: movie.medium_cover_image,
-				id: movie.id,
-				titre: movie.title,
-				year: movie.year,
-				runtime: movie.runtime,
-				rating: movie.rating,
-				language: movie.language,
-				genres: movie.genres,
-				description: movie.description_full,
-				imdbCode: movie.imdb_code,
-			}))
-			const listFavorite = await getFavorites(req.user, 'imdbID')
-			const countWatchedMovies = await getCountWatchedMovie(movieInfo.imdb_code)
-			return res.send({
-				type: 'success',
-				status: 200,
-				body: {
-					titleLong: movieInfo.title_long,
-					title: movieInfo.title,
-					year: movieInfo.year,
-					imdbCode: movieInfo.imdb_code,
-					cast: credits && credits.data.cast instanceof Array ? credits.data.cast : [],
-					description: movieInfo.description_full,
-					genres: movieInfo.genres,
-					id: movieInfo.id,
-					language: movieInfo.language,
-					postImage: images ? `https://image.tmdb.org/t/p/original/${images.data.posters[0].file_path}` : '',
-					coverImage:
-						images && images.data.backdrops.length && images.data.backdrops[0].hasOwnProperty('file_path')
-							? `https://image.tmdb.org/t/p/original/${images.data.backdrops[0].file_path}`
-							: '',
-					runtime: movieInfo.runtime,
-					codeTrailer: movieInfo.yt_trailer_code,
-					screenshotImage: [movieInfo.large_screenshot_image1, movieInfo.large_screenshot_image2, movieInfo.large_screenshot_image3],
-					suggestions,
-					torrents: movieInfo.torrents.map((item) => ({ quality: item.quality, hash: item.hash, type: item.type })),
-					rating: movieInfo.rating,
-					dateUploaded: movieInfo.date_uploaded,
-					isFavorite: listFavorite instanceof Array && listFavorite.findIndex((a) => a.imdbID === movieInfo.imdb_code) !== -1 ? true : false,
-					countWatchedMovies: countWatchedMovies,
-				},
-			})
+			if (codeMovie.data.data.movies) {
+				const movieData = await axios.get(`https://yts.megaproxy.info/api/v2/movie_details.json?movie_id=${codeMovie.data.data.movies[0].id}&with_images=true`)
+				const movieInfo = movieData.data.data.movie
+				const { images, credits } = await getMovieImagesAndCasts(imdbID)
+				const listFavorite = await getFavorites(req.user, 'imdbID')
+				const countWatchedMovies = await getCountWatchedMovie(movieInfo.imdb_code)
+				return res.send({
+					type: 'success',
+					status: 200,
+					body: {
+						titleLong: movieInfo.title_long,
+						title: movieInfo.title,
+						year: movieInfo.year,
+						imdbCode: movieInfo.imdb_code,
+						cast: credits && credits.data.cast instanceof Array ? credits.data.cast : [],
+						description: movieInfo.description_full,
+						genres: movieInfo.genres,
+						id: movieInfo.id,
+						language: movieInfo.language,
+						postImage: images ? `https://image.tmdb.org/t/p/original/${images.data.posters[0].file_path}` : '',
+						coverImage:
+							images && images.data.backdrops.length && images.data.backdrops[0].hasOwnProperty('file_path')
+								? `https://image.tmdb.org/t/p/original/${images.data.backdrops[0].file_path}`
+								: '',
+						runtime: movieInfo.runtime,
+						codeTrailer: movieInfo.yt_trailer_code,
+						screenshotImage: [movieInfo.large_screenshot_image1, movieInfo.large_screenshot_image2, movieInfo.large_screenshot_image3],
+						torrents: movieInfo.torrents.map((item) => ({ quality: item.quality, hash: item.hash, type: item.type })),
+						rating: movieInfo.rating,
+						dateUploaded: movieInfo.date_uploaded,
+						isFavorite: listFavorite instanceof Array && listFavorite.findIndex((a) => a.imdbID === movieInfo.imdb_code) !== -1 ? true : false,
+						countWatchedMovies: countWatchedMovies,
+					},
+				})
+			}
 		}
 		return res.send({
 			type: 'error',
@@ -91,6 +78,8 @@ const getMovieInfoBackup = async function (req, res, next) {
 		const { images, credits } = await getMovieImagesAndCasts(imdbID)
 		const listFavorite = await getFavorites(req.user, 'imdbID')
 		const countWatchedMovies = await getCountWatchedMovie(movieInfo.imdb)
+		const torrents = []
+		movieInfo.items.map((item, index) => (index <= 5 ? torrents.push({ quality: item.quality, hash: item.id, type: '' }) : null))
 		return res.send({
 			type: 'success',
 			status: 200,
@@ -112,8 +101,7 @@ const getMovieInfoBackup = async function (req, res, next) {
 				runtime: movieInfo.runtime,
 				codeTrailer: movieInfo.trailer,
 				screenshotImage: [],
-				suggestions: [],
-				torrents: movieInfo.items.map((item, index) => (index <= 5 ? { quality: item.quality, hash: item.id, type: '' } : null)),
+				torrents,
 				rating: movieInfo.rating,
 				isFavorite: listFavorite instanceof Array && listFavorite.findIndex((a) => a.imdbID === movieInfo.imdb) !== -1 ? true : false,
 				countWatchedMovies: countWatchedMovies,
