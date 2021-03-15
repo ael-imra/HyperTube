5cfd7f5fdbe2d31dcd8503aaf1dd6fb38e3008dd
Soufiane El hamri <sel-hamr@e2r1p1.1337.ma>
2021-03-15 19:01:28 +0100

    last push

--- server/middleware/errorHandler.js
+++ server/middleware/errorHandler.js
@@ -1,11 +1,10 @@
 const errorHandler = function (err, req, res, next) {
-	console.log(err)
-	if (err)
-		res.send({
-			type: 'error',
-			status: 403,
-			body: { Eng: 'Something wrong please try again', Fr: 'Un problème est survenu, veuillez réessayer' },
-		})
-	else next()
-}
-module.exports = errorHandler
+    if (err)
+        res.send({
+            type: 'error',
+            status: 403,
+            body: { Eng: 'Something wrong please try again', Fr: 'Un problème est survenu, veuillez réessayer' },
+        });
+    else next();
+};
+module.exports = errorHandler;
