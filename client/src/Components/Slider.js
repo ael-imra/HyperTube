4cf8461e2bc50e1ce648b38ac93e5ca6beb172c1
ael-imra <abd123abdomen54@hotmail.com>
2022-11-14 23:09:56 +0100

    change hardcode url host

--- client/src/Components/Slider.js
+++ client/src/Components/Slider.js
@@ -1,10 +1,11 @@
-import React from "react";
-import NavigateBeforeIcon from "@material-ui/icons/NavigateBefore";
-import NavigateNextIcon from "@material-ui/icons/NavigateNext";
-import "../Css/Slider.css";
-import Axios from "axios";
-import { UseWindowSize } from "../Assets/UseWindowSize";
-import { CartSlider } from "./CartSlider";
+import React from 'react';
+import NavigateBeforeIcon from '@material-ui/icons/NavigateBefore';
+import NavigateNextIcon from '@material-ui/icons/NavigateNext';
+import '../Css/Slider.css';
+import Axios from 'axios';
+import { UseWindowSize } from '../Assets/UseWindowSize';
+import { CartSlider } from './CartSlider';
+import { HOST } from '../constants';
 export default function Slider() {
   const width = UseWindowSize();
 
@@ -14,13 +15,19 @@ export default function Slider() {
   React.useEffect(() => {
     let unmount = false;
     async function leakData() {
-      const popularMovies = await Axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=7a518fe1d1c5359a4929ef4765c347fb`);
-      const listFavorite = await Axios(`http://localhost:1337/favorite/imdbID`, { withCredentials: true });
+      const popularMovies = await Axios.get(
+        `https://api.themoviedb.org/3/movie/popular?api_key=7a518fe1d1c5359a4929ef4765c347fb`
+      );
+      const listFavorite = await Axios(`${HOST}/favorite/imdbID`, { withCredentials: true });
       const detailPopularMovies = await new Promise((resolve) => {
         const result = [];
         popularMovies.data.results.map(async (movie) => {
-          const image = await Axios.get(`https://api.themoviedb.org/3/movie/${movie.id}/images?api_key=7a518fe1d1c5359a4929ef4765c347fb`);
-          const getImdbCode = await Axios.get(`https://api.themoviedb.org/3/movie/${movie.id}/external_ids?api_key=7a518fe1d1c5359a4929ef4765c347fb`);
+          const image = await Axios.get(
+            `https://api.themoviedb.org/3/movie/${movie.id}/images?api_key=7a518fe1d1c5359a4929ef4765c347fb`
+          );
+          const getImdbCode = await Axios.get(
+            `https://api.themoviedb.org/3/movie/${movie.id}/external_ids?api_key=7a518fe1d1c5359a4929ef4765c347fb`
+          );
           image.data.backdrops.sort((a, b) => b.width - a.width);
           result.push({
             id: movie.id,
@@ -30,7 +37,11 @@ export default function Slider() {
             overview: movie.overview,
             date: parseInt(movie.release_date),
             rating: movie.vote_average,
-            isFavorite: listFavorite.data.body instanceof Array && listFavorite.data.body.findIndex((a) => a.imdbID === getImdbCode.data.imdb_id) !== -1 ? true : false,
+            isFavorite:
+              listFavorite.data.body instanceof Array &&
+              listFavorite.data.body.findIndex((a) => a.imdbID === getImdbCode.data.imdb_id) !== -1
+                ? true
+                : false,
             imdbCode: getImdbCode.data.imdb_id,
           });
           if (result.length === popularMovies.data.results.length) resolve(result);
@@ -42,16 +53,23 @@ export default function Slider() {
     return () => (unmount = true);
   }, []);
   return width > 600 ? (
-    <div className='Slide'>
+    <div className="Slide">
       {sliderActive !== 0 ? (
         <NavigateBeforeIcon
           onClick={() => {
             setSliderActive(sliderActive === 0 ? listPopularMovies.length - 1 : sliderActive - 1);
           }}
-          style={{ fontSize: "55px", cursor: "pointer", position: "absolute", left: "25px", zIndex: "1", color: "white" }}
+          style={{
+            fontSize: '55px',
+            cursor: 'pointer',
+            position: 'absolute',
+            left: '25px',
+            zIndex: '1',
+            color: 'white',
+          }}
         />
       ) : (
-        ""
+        ''
       )}
       {listPopularMovies.map((movie, key) => (
         <CartSlider key={key} index={key} movie={movie} sliderActive={sliderActive} />
@@ -61,13 +79,20 @@ export default function Slider() {
           onClick={() => {
             setSliderActive(sliderActive === listPopularMovies.length - 1 ? 0 : sliderActive + 1);
           }}
-          style={{ fontSize: "55px", cursor: "pointer", position: "absolute", right: "25px", zIndex: "1", color: "white" }}
+          style={{
+            fontSize: '55px',
+            cursor: 'pointer',
+            position: 'absolute',
+            right: '25px',
+            zIndex: '1',
+            color: 'white',
+          }}
         />
       ) : (
-        ""
+        ''
       )}
     </div>
   ) : (
-    ""
+    ''
   );
 }
--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./Components/App";
-import Axios from "axios";
-Axios.defaults.baseURL = "http://localhost:1337";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './Components/App';
+import Axios from 'axios';
+import { HOST } from './constants';
+Axios.defaults.baseURL = HOST;
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById('root'));
